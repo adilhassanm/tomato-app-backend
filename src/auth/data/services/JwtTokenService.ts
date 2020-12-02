5392b2e48f6e32038f4dbd66db4fb1cae97af4ec
Adil Hassan <adil27jan@gmail.com>
2020-12-02 16:25:55 +0400

    enter

--- src/auth/data/services/JwtTokenService.ts
+++ src/auth/data/services/JwtTokenService.ts
@@ -2,11 +2,18 @@ import ITokenService from "../../services/ITokenService";
 import jwt from 'jsonwebtoken'
 
 export default class JwtTokenService implements ITokenService{
+    constructor(private readonly privateKey:string){}
     encode(payload: string | object): string | object {
-        throw new Error("Method not implemented.");
+       
+        let token = jwt.sign({data:payload},this.privateKey,{issuer:'com.fodapp',expiresIn:'1h'})
+        return token
     }
-    decode(token: string | object): string | object {
-        throw new Error("Method not implemented.");
+    decode(token: string ): string | object {//token cannot be object but a string
+        try {
+            const decoded = jwt.verify(token,this.privateKey)
+            return decoded
+            
+        } catch (error) {return 'Invallid Token'}
     }
 
 }
